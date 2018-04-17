ec3a8fdc992fbed5c151f6965b41da345f9f5443
Alex Johnson <notalexjohnson@gmail.com>
2018-04-17 16:16:12 +0530

    Update test dependencies and fix setup

--- ghtorrent.gemspec
+++ ghtorrent.gemspec
@@ -36,6 +36,9 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'simplecov-rcov'
   s.add_development_dependency 'mocha'
+  s.add_development_dependency 'byebug'
+  s.add_development_dependency 'vcr'
+  s.add_development_dependency 'webmock'
 
 
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,18 @@
 require 'simplecov'
 require 'simplecov-rcov'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-SimpleCov.start 
+SimpleCov.start
 SimpleCov.minimum_coverage 96.3
 
+require 'vcr'
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
 
 require 'minitest/autorun'
 require 'ghtorrent'
-require 'mocha/mini_test'
+require 'mocha/minitest'
 require 'byebug'
 
 
